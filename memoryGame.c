43c8c3e2cbab5fbd0f061dd0b9d4bff4ea786a3b
codeahl <142953446+codeahl@users.noreply.github.com>
2023-11-20 14:11:06 -0600

    cool

--- memoryGame.c
+++ memoryGame.c
@@ -21,7 +21,7 @@ Team member 4 "Name" | "Percentage of Contribution to The Project"
 /////////////////////////////////
 int main(){
 return 0;
-//Apples
+//Apple
 }
 ///////////////////////////////////////
 //User Defined Functions' Definition//
